35b3df9e5c5db463a18f59426128bf9c4e648ec0
Simran <simran.develop@gmail.com>
2022-11-08 14:15:13 +1300

    basic hero section added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    fontLoaders: [
+      {
+        loader: '@next/font/google',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
